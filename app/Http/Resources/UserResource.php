52c7e256141571e7e94b81c98d5238dad8daceb7
Albertj PG <ajpg19@gmail.com>
2023-03-06 00:49:05 +0100

    create user resource

---
+++ app/Http/Resources/UserResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class UserResource extends JsonResource
+{
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "email" => $this->email,
+            "city" => $this->city,
+            "posts" => PostResource::collection($this->whenLoaded("posts"))
+        ];
+    }
+}
